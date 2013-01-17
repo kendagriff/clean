b2da8a26f088246fef7f0b4fccafbacf1b418b4f
Chase Dagley <chasedagley9@hotmail.com>
2013-01-16 18:34:32 -0700

    what is done so far

--- clean.rb
+++ clean.rb
@@ -1,19 +1,63 @@
+require "fileutils"
+
 commands = ["data", "role", "context"]
 
-case ARGV[0]	
+# Make my array
+directories = ARGV[1].split(":")
+
+case ARGV[0]
 when "data"
-	puts "running data"
+	# file = "user_test.rb"
+	line_1 = puts "setup do"
+	line_2 = 'test "do somethin" do'
+	file = directories.pop
+	file_path = "#{file}_test.rb" 
+	directory_path = "test/unit/#{directories.join('/')}"	
+	file_contents = <<-EOS
+require 'test_helper'
+EOS
+	# Module keywords
+	directories.each_with_index do |mod, i|
+		i.times { file_contents << "  "}
+		file_contents << "module #{mod.capitalize}\n"
+	end
+
+	# Class
+	directories.length.times { file_contents << "  " }
+	file_contents << "class #{file.capitalize}Test < ActiveSupport::TestCase\n"
+	directories.length.times { file_contents << "  " }
+	file_contents << "end\n"
+
+	i.times { line_1 << "  "}
+	i = puts.length - i - 1
+
+	i.times { line_2 << "  "}
+	i = puts.length - i - 1
+
+	# Module ends
+	directories.each_with_index do |mod, i|
+		i = directories.length - i - 1
+		i.times { file_contents << "  " }
+		file_contents << "end\n"
+	end
+
+	FileUtils.mkdir_p directory_path
+	File.open("#{directory_path}/#{file_path}", "w") do |f|
+		f.puts file_contents
+	end
+	puts "changes saved"
 when "role"
-	puts "running role"
+	puts "changes saved"
 when "context"
-	puts "running context"
+	puts "changes saved"
 else
 	puts "command not found :("
 end
+
 
 #collect the command
 #needs to run commands
 #commands are data, role, and context
 #tell me if entered a wrong command
 #tell me if entered right command
-#need to spit out files with pre-generated code depending on the command
+#needs to spit out files with pre-generated code depending on the command
